53c161e531f033651903e58bdd8e5ef96e70e91d
Jorge <jadr.whisky@gmail.com>
2026-03-01 11:34:09 -0400

    ganaconjuvenil v0.0.2 ajuste de descripcion Sorteo1

--- src/data/rifas.js
+++ src/data/rifas.js
@@ -19,14 +19,20 @@ export const listaRifas = [
 Estamos lanzando nuestro gran sorteo con premios que no puedes dejar pasar:
 
 🏆 1er Lugar: ¡Una espectacular Moto 0km! 🏍️
+🥈 2do Lugar: Equivalente a $600 en Bs. 💵
+🥉 3er Lugar: Equivalente a $400 en Bs. 💵
 
-🥈 2nd Lugar: Equivalente $600 en Bs. 💵
+✨ Premios Especiales: Estaremos repartiendo 4 premios equivalentes a $50 en Bs. c/u para tickets ganadores al azar. 🎫💸
 
-🥉 3rd Lugar: Equivalente $400 en Bs. 💵
+👑 ¡PREMIOS EXTRA A LOS MAYORES COMPRADORES! 👑
 
-✨ Premios Especiales: Estaremos repartiendo 4 premios equivalente a $50 en Bs. c/u para tickets ganadores. 💸
+Para agradecer tu lealtad, premiaremos a las 3 personas que compren la mayor cantidad de boletos en toda la rifa:
 
-¡Participar es súper fácil!`,
+🥇 Top 1 en compras: Equivalente a $200 en Bs.
+🥈 Top 2 en compras: Equivalente a $125 en Bs.
+🥉 Top 3 en compras: Equivalente a $75 en Bs.
+
+¡Participar es súper fácil! Mientras más tickets adquieras, más oportunidades tienes de ganar la moto y de asegurar tu puesto en el podio de compradores. 🎟️🚀`,
     estado: "activa", // <--- Controla si se vende o no
   },
 ];
